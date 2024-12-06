e61f8aa09a2c7ed71f76f518b2096da3413509a5
Grocery4 <juddrussel.kalalo@gmail.com>
2024-12-06 17:17:52 +0100

    transformed function into class

--- FUNZIONI-sistema_di_prenotazione.py
+++ FUNZIONI-sistema_di_prenotazione.py
@@ -3,23 +3,11 @@
 # Utilizzeremo tutti i concetti sopra descritti per costruire questo esempio.
 
 from enum import Enum
-
 class Camera(Enum):
     ECONOMY = 0
     STANDARD = 1
     BUSINESS = 2
     LUXURY = 3
-    
-
-class Prenotazione:
-	def __init__(self, nc, tc, ds, *oe, **dp):
-		self.nc = nc
-		self.tc = tc
-		self.ds = ds
-		self.oe = oe # tuples
-		self.dp = dp # dictionary
-
-		calcola_prezzi() # TODO
 
 def prenota_camera(nome_cliente, tipo_camera = Camera.STANDARD, durata_soggiorno = 3, *opzioni_extra, **dettagli_pagamento):
 
@@ -51,6 +39,43 @@ def prenota_camera(nome_cliente, tipo_camera = Camera.STANDARD, durata_soggiorno
 	if dettagli_pagamento != None:
 		for dt, val in dettagli_pagamento.items():
 			print("- ", dt, ": ", val)
-	
 
-prenota_camera("Judd Russel Kalalo", Camera.LUXURY, 5, "Bagno Privato", "Vista panoramica", "Servizio in camera", metodo="Carta di Credito", numero_transazione = 1137124)
+class Prenotazione:
+	def __init__(self, nc, ds, tc = Camera.STANDARD, *oe, **dp):
+		self.nc = nc
+		self.tc = tc
+		self.ds = ds
+		self.oe = oe # tuples
+		self.dp = dp # dictionary
+
+		self.calcola_prezzi()
+		self.stampa_prenotazione()
+
+	def calcola_prezzi(self):
+		self.prezzo = 0
+		if self.tc == Camera.LUXURY:
+			self.prezzo = 150
+		else:
+			self.prezzo = 100
+
+		for op in self.oe:
+			self.prezzo += 10
+
+
+	def stampa_prenotazione(self):
+		print("Riepilogo prenotazione:")
+		print("Costo totale: ", self.prezzo, "€")
+
+		print('\n', "Tipologia camera: ")
+		print("-", self.tc.name)
+
+		print('\n', "Opzioni extra:")
+		for op in self.oe:
+			print("- ", op)	
+
+		print('\n', "Dettagli pagamento:")
+		if self.dp != None:
+			for dt, val in self.dp.items():
+				print("- ", dt.capitalize().replace("_", " "), ": ", val)
+
+Prenotazione("Judd Russel Kalalo", 5, Camera.LUXURY, "Bagno Privato", "Vista panoramica", "Servizio in camera", metodo="Carta di Credito", numero_transazione = 1137124)
